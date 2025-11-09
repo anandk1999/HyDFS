2f20186d957c8825fce471e67e7711904a3903e8
anandk1999 <ksaikarteek2003@gmail.com>
2025-11-09 14:00:24 -0600

    change

--- scripts/test1_create.sh
+++ scripts/test1_create.sh
@@ -17,10 +17,7 @@ SLEEP_AFTER_CREATE=3
 run_on() {
   local host="$1"; shift
   if [[ "$host" == "localhost" || "$host" == "127.0.0.1" ]]; then
-    for arg in $@; do
-      echo $arg
-    done
-    (cd "/home/saik2/mp3-g02" && "$@")
+    (cd "/home/saik2/mp3-g02" && echo ls && "$@")
   else
     ssh "$host" "cd /home/saik2/mp3-g02 && $*"
   fi
