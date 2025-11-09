adcfa233a5b89431293b742baf6370135b93f81c
anandk1999 <ksaikarteek2003@gmail.com>
2025-11-09 13:52:35 -0600

    change

--- scripts/test1_create.sh
+++ scripts/test1_create.sh
@@ -17,8 +17,7 @@ SLEEP_AFTER_CREATE=3
 run_on() {
   local host="$1"; shift
   if [[ "$host" == "localhost" || "$host" == "127.0.0.1" ]]; then
-    ./client
-    (cd "$(pwd)" && "$@")
+    (cd "/home/saik2/mp3-g02" && "$@")
   else
     ssh "$host" "cd /home/saik2/mp3-g02 && $*"
   fi
